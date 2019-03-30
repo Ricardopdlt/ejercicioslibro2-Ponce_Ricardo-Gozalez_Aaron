c7e0f4ee27e2457cf06a557da76f8d2aba9c875b
Ricardo Ponce <joaqopdlt@gmail.com>
2019-03-30 18:51:23 -0500

    primer commit

---
+++ Ejercicio2.16.cpp
@@ -0,0 +1,26 @@
+//Ejercicio 2.16
+//Escriba un programa que pida al usuario que escriba dos números, obtenga esos dos números del usuario e imprima la suma, producto, diferencia y cociente de los dos números.
+
+#include <stdio.h>
+#include <conio.h>	
+
+using namespace System;
+using namespace std;
+int main()
+{
+	printf("Programa que calcula las operaciones basicas con 2 numeros enteros\n");
+	int x, y, r1, r2, r3, r4;
+	printf("Escriba el primer numero:\n");
+	scanf("%d", &x);
+	printf("Escriba el segundo numero:\n");
+	scanf("%d", &y);
+	r1 = x + y;
+	r2 = x * y;
+	r3 = x - y;
+	r4 = x / y;
+	printf("El resultado de la suma es %d\n", r1);
+	printf("El resultado de la multiplicacion es %d\n", r2);
+	printf("El resultado de la resta es %d\n", r3);
+	printf("El resultado de la division es %d\n", r4);
+	_getch();
+}
