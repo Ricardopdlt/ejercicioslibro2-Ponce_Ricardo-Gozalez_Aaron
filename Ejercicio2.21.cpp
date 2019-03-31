d0648a6d06242a8f4fe49c3c6f02255081ccef23
Ricardo Ponce <joaqopdlt@gmail.com>
2019-03-30 23:17:47 -0500

    primer commit

---
+++ Ejercicio2.21.cpp
@@ -0,0 +1,23 @@
+//Ejercicio 2.21
+//Escriba un programa que imprima una caja, un óvalo, una flecha y un diamante.
+
+#include <stdio.h>
+#include <conio.h>
+
+using namespace System;
+using namespace std;
+
+int main()
+{
+	printf("*********         ***         *            *\n");
+	printf("*       *       *     *      ***          * *\n");
+	printf("*       *      *       *    *****        *   *\n");
+	printf("*       *      *       *      *         *     *\n");
+	printf("*       *      *       *      *	       *       *\n");
+	printf("*       *      *       *      *         *     *\n");
+	printf("*       *      *       *      *          *   *\n");
+	printf("*       *       *     *       *           * *\n");
+	printf("*********         ***         *            *\n");
+	_getch();
+
+}
