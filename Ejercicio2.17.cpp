d6f82f75c808f78847f0df9e2711c72d1913e9c4
Ricardo Ponce <joaqopdlt@gmail.com>
2019-04-01 16:48:51 -0500

    primer commit

---
+++ Ejercicio2.17.cpp
@@ -0,0 +1,30 @@
+//Ejercicio 2.17
+//Imprimir los nùmeros del 1 al 4 dejando entre sus adyacentes de cada uno un espacio
+
+#include <iostream>
+#include <conio.h>
+
+using namespace System;
+using namespace std;
+
+int main()
+{//a) una instrución con un operdador de inserción de flujo
+
+	cout << " 1 2 3 4  \n ";
+
+// b) una instrución con cuatro operadores de inserción de flujo
+
+	cout << "1" << " 2" << " 3" << " 4\n";
+
+// c) cuatro instrucciones
+
+	cout << " 1";
+	cout << " 2";
+	cout << " 3";
+	cout << " 4\n";
+
+	_getch();
+
+}
+
+
